0987ef5dd5a88a0defbdd116f798f9be317fac78
Stefano Lambiase <stefanolambiase96@gmail.com>
2021-03-06 18:49:45 +0100

    bug fix

--- app/main.js
+++ app/main.js
@@ -15,7 +15,7 @@ if (require('electron-squirrel-startup')) {
  */
 const createWindow = () => {
   // Create the browser window.
-  const window = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -27,10 +27,10 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  window.loadFile(path.join(__dirname, 'rendererProcess/html/index.html'));
+  mainWindow.loadFile(path.join(__dirname, 'rendererProcess/html/index.html'));
 
   // Open the DevTools.
-  window.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 };
 
 
@@ -41,7 +41,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => createWindow);
+app.on('ready', createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
