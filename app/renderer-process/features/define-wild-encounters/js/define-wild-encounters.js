30b9d4622310ee944fa308cd85efe9a7e0262f5b
Stefano Lambiase <stefanolambiase96@gmail.com>
2021-03-21 17:18:13 +0100

    implement graphic for define-wild-encounters

--- app/renderer-process/features/define-wild-encounters/js/define-wild-encounters.js
+++ app/renderer-process/features/define-wild-encounters/js/define-wild-encounters.js
@@ -4,9 +4,14 @@
 
 // ! IMPORTANT: the path starts from the renderer-process directory.
 const thisfeaturesPath = featuresPath;
-const componentsPath = thisfeaturesPath + 'define-wild-encounters/components/'
+const componentsPath = thisfeaturesPath + 'define-wild-encounters/components/entities/'
 
 const entities12Path = componentsPath + 'entities-12.html';
+const entitiesWaterRockPath = componentsPath + 'entities-water-rock.html';
+const entitiesOldRodPath = componentsPath + 'entities-old-rod.html';
+const entitiesGoodRodPath = componentsPath + 'entities-good-rod.html';
+const entitiesSuperRodPath = componentsPath + 'entities-super-rod.html';
+const entitiesHeadbuttPath = componentsPath + 'entities-headbutt.html';
 
 
 // * ------------------------------------------------------------------------------------
@@ -32,7 +37,10 @@ function initDefineWildEncountersForm() {
         './features/define-wild-encounters/components/define-wild-encounters-form.html',
         undefined,
         () => {
-            initLandPart();
+            initLandCaveBugPart();
+            initWaterRockPart();
+            initRodPart();
+            initHeadbutt();
         }
     )
 
@@ -40,10 +48,73 @@ function initDefineWildEncountersForm() {
 }
 
 
-function initLandPart() {
-    $('#wildEncountersLand').load(
-        entities12Path,
+function initLandCaveBugPart() {
+    let elements = [
+        document.getElementById('wildEncountersLand'),
+        document.getElementById('wildEncountersLandMorning'),
+        document.getElementById('wildEncountersLandDay'),
+        document.getElementById('wildEncountersLandNight'),
+        document.getElementById('wildEncountersCave'),
+        document.getElementById('wildEncountersBugContest'),
+    ];
+
+    for (let element of elements) {
+        $(element).load(
+            entities12Path,
+            undefined,
+            undefined
+        );
+    }
+}
+
+
+function initWaterRockPart() {
+    let elements = [
+        document.getElementById('wildEncountersWater'),
+        document.getElementById('wildEncountersRockSmash'),
+    ];
+
+    for (let element of elements) {
+        $(element).load(
+            entitiesWaterRockPath,
+            undefined,
+            undefined
+        );
+    }
+}
+
+
+function initRodPart() {
+    $('#wildEncountersOldRod').load(
+        entitiesOldRodPath,
         undefined,
         undefined
-    )
+    );
+
+    $('#wildEncountersGoodRod').load(
+        entitiesGoodRodPath,
+        undefined,
+        undefined
+    );
+
+    $('#wildEncountersSuperRod').load(
+        entitiesSuperRodPath,
+        undefined,
+        undefined
+    );
+}
+
+
+function initHeadbutt() {
+    $('#wildEncountersHeadbuttLow').load(
+        entitiesHeadbuttPath,
+        undefined,
+        undefined
+    );
+
+    $('#wildEncountersHeadbuttHigh').load(
+        entitiesHeadbuttPath,
+        undefined,
+        undefined
+    );
 }
