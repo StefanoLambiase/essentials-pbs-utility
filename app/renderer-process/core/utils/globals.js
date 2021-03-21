40df50e59f9fdba84a35d4e2c192561e56338e38
Stefano Lambiase <stefanolambiase96@gmail.com>
2021-03-21 10:20:01 +0100

    bug fix

--- app/renderer-process/core/utils/globals.js
+++ app/renderer-process/core/utils/globals.js
@@ -2,4 +2,5 @@
 window.failAlert = document.getElementById('failAlert');
 window.successAlert = document.getElementById('successAlert');
 
+// The Path begin from the index.html file.
 window.featuresPath = './features/'
